7a701bcfd6eb690ce8ff481f946992f1a8ab6d7e
EC2 Default User <ec2-user@ip-172-31-31-80.us-east-2.compute.internal>
2021-06-13 15:01:08 +0000

    Rails04

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{8,32}\z/i
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX } 
+  validates :password_confirmation, presence: true, format: { with: VALID_PASSWORD_REGEX }
+  
+  has_secure_password
+end
